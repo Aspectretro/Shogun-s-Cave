443c20d06a9ef4a9a4062ee14df412bd4b5dd4bf
Aspectretro <max.zhengsc10@gmail.com>
2025-06-03 11:29:19 +1000

    chore: add libary imports and file links

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import game
+
+
+def main():
+    pass # TODO: Add items such as tutorial(help)
+
+def update():
+    pass # TODO: Game and character update goes under here. This is also a main loop.
