2486fb2d3181f059eef0a4f208aa13279866cec4
azyrite <azyrite@astrial.org>
2025-06-03 11:32:25 +1000

    feat: implemented basic map structure

--- src/map.py
+++ src/map.py
@@ -1,4 +1,21 @@
+from cave import Cave
+
 class Map:
     """A class that represents the map of caves in the game
     """
-    map = {} # TODO: complete floor plan by the end of this week. Potentially using numbers instead of directions to move around
+    
+    def __init__(self):
+        self.map = {}
+
+    def generate(self):
+        """Fill in the map with new caves, overwriting the current map"""
+        # TODO: names and descriptions
+        cave1 = Cave(1, "Cave", "A small cave underground. A little light seeps in from a crack in the ceiling")
+        cave2 = Cave(2, "Grotto", "A cramped and damp recess. Maybe it would be a great place to hide...")
+        cave3 = Cave(3, "Cavern", "A large, spacious cavern")
+
+        self.map = {
+            cave1: cave2,
+            cave2: cave3,
+            cave3: cave1
+        }
