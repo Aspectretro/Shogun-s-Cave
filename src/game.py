96a999590f022656a6569370c87e1066ab142d99
azyrite <azyrite@astrial.org>
2025-06-03 11:32:43 +1000

    feat: some impl for Game

--- src/game.py
+++ src/game.py
@@ -1,7 +1,18 @@
+from map import Map
+
 class Game:
-    """Class that manages all aspects of a currently running session
+    """Class that manages all aspects of a currently running session, including the player 
 
     All other classes are managed by an instance of this class
     """
     def __init__(self):
-        pass
+        self.alive = True
+
+        # Generate map
+        self.map = Map()
+        self.map.generate()
+
+    def start(self):
+        """Entrypoint for the game. Starts the game loop"""
+        while self.alive:
+            pass
