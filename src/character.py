439c32788d7cb3b5e14cca1ded39d7cb984d3d66
azyrite <azyrite@astrial.org>
2025-06-25 11:24:10 +1000

    fix: do not reimpl inherited attrs

--- src/character.py
+++ src/character.py
@@ -86,15 +86,9 @@ class Ninja(Enemy):
 
 
 class Friendly(Character):
-    def __init__(self, name, description):
-        super().__init__(name, description) # initialise super class
+    def __init__(self, name, description, cave):
+        super().__init__(name, description, cave) # initialise super class
         self.conversation = None
-    
-    def set_description(self, new_desc):
-        self.description = new_desc
-    
-    def get_description(self):
-        return self.description
     
     def shop(self, item):
         pass # Potential addons by group
