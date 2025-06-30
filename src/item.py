76bbce7c06c1161fe1c6fb864bfa4eecc30a9afb
azyrite <azyrite@astrial.org>
2025-06-30 12:34:48 +1000

    fix: Item class getters, item damage

--- src/item.py
+++ src/item.py
@@ -2,24 +2,33 @@ class Item:
     """Represents an item in the game
     """
 
-    def __init__(self, name, emoji, description, cost):
+    def __init__(self, name, emoji, description, cost, damage=0):
         self.name = name
         self.emoji = emoji
         self.description = description
         self.cost = cost
+        self.damage = damage # damage the item does
 
-    def get_display():
+    def get_display(self):
         """Return the name of the item along with its representative emoji"""
         return f"{self.emoji} {self.name}"
 
-    def get_name():
+    def get_name(self):
         """Returns the item's name"""
         return self.name
 
-    def get_description():
+    def get_description(self):
         """Return the item's description"""
         return self.description
     
-    def get_cost():
+    def get_cost(self):
         """Return this item's cost"""
         return self.cost
+
+    def get_damage(self):
+        """Return this item's damage"""
+        return self.damage 
+
+    def set_damage(self, new_damage):
+        """Set this item's damage"""
+        self.damage = new_damage
